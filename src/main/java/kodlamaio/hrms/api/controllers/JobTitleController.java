929bfd253a85a19fa6b2272f68be340f7615d87e
Sema Salayeva <semasalayeva95@gmail.com>
2021-05-22 22:54:36 +0400

    hrms project added

--- src/main/java/kodlamaio/hrms/api/controllers/JobTitleController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/JobTitleController.java
@@ -2,7 +2,7 @@ package kodlamaio.hrms.api.controllers;
 
 import kodlamaio.hrms.busniess.abstracts.JobTitleService;
 import kodlamaio.hrms.entities.JobTitle;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +14,7 @@ import java.util.List;
 public class JobTitleController {
     private JobTitleService jobTitleService;
 
+    @Autowired
     public JobTitleController(JobTitleService jobTitleService) {
         this.jobTitleService = jobTitleService;
     }
